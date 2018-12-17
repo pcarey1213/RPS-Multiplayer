a01bfda7028cc2cda278d3f6f38708cdd2f926d6
Preston Carey <pcarey1213@gmail.com>
2018-12-17 14:35:56 -0500

    first hw commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+
+var config = {
+apiKey: "AIzaSyDQrTJbbEJOltv_EDzXu5R8ehU7AqtG3Sc",
+authDomain: "rps-multiplayer-7ac9c.firebaseapp.com",
+databaseURL: "https://rps-multiplayer-7ac9c.firebaseio.com",
+projectId: "rps-multiplayer-7ac9c",
+storageBucket: "",
+messagingSenderId: "990066240414"
+};
+
+firebase.initializeApp(config);
